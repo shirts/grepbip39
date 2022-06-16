e61bccf5faa99025832e2ef1b2a0c1760c165459
Kevin C <im.kevin@icloud.com>
2022-06-15 17:11:58 -0700

    Initial commit - search

---
+++ README.md
@@ -0,0 +1,4 @@
+# minigrep
+
+An implementation of the Rust book's [I/O Project](https://doc.rust-lang.org/stable/book/ch12-00-an-io-project.html)
+
---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config<'a> {
+    query: &'a str,
+    filename: &'a str
+}
+
+impl<'a> Config<'a> {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        Ok(Config {
+            query: &args[1],
+            filename: &args[2]
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)
+        .expect(&format!("Could not read {}", &config.filename));
+
+    println!("{}", contents);
+
+    Ok(())
+}
+
+// search a word from a vector
+pub fn search(word: &str, contents: &Vec<&str>) -> bool {
+    for &line in contents {
+        if line == word {
+            println!("Found word {}", word);
+            return true
+        }
+    }
+
+    false
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_finds_word_true() {
+        let contents = vec!["apple", "banana", "zoo"];
+
+        assert_eq!(search("zoo", &contents), true);
+    }
+
+    #[test]
+    fn search_finds_word_false() {
+        let contents = vec!["apple", "banana", "zoo"];
+
+        assert_eq!(search("pear", &contents), false);
+    }
+}
